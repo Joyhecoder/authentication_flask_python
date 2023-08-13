1fed464040c5fc5135fd4377a5c0cf6594842566
Your GitHub Username <joyhe520@gmail.com>
2023-08-13 16:21:13 -0500

    base html

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 #! initialize blueprint, always need to pass in __name__, and the name of the blueprint
 # views = Blueprint(__name__, "views")
@@ -7,4 +7,4 @@ views = Blueprint("views", __name__)
 
 @views.route("/")
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
